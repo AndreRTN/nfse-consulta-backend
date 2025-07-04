5502cb5a0e5bb96a97fa5445c1f7809bc4d3ef06
André Roberto <andredocin2000@gmail.com>
2025-07-04 13:57:41 -0300

    spring initializr setup

---
+++ src/main/java/com/challenge/credit_consultation/CreditConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.credit_consultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditConsultationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditConsultationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/credit_consultation/CreditConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.credit_consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
