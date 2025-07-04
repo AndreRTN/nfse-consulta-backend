87e533661a9b9eaf14bcfbebde36f6496f1f6c39
André Roberto <andredocin2000@gmail.com>
2025-07-04 14:15:27 -0300

    credit model

---
+++ src/main/java/com/challenge/credit_consultation/common/entity/Credito.java
@@ -0,0 +1,32 @@
+package com.challenge.credit_consultation.common.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table
+public class Credito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numeroCredito;
+    private String numeroNfse;
+    private LocalDate dataConstituicao;
+    private BigDecimal valorIssqn;
+    private String tipoCredito;
+    private boolean simplesNacional;
+    private BigDecimal aliquota;
+    private BigDecimal valorFaturado;
+    private BigDecimal valorDeducao;
+    private BigDecimal baseCalculo;
+
+}
